763153f67a29046c3811ababbe71ad3b1f397b35
Aaron Gankin <agankin02@gmail.com>
2020-01-07 08:49:31 -0500

    Added Ball and Camera Switch Scripts

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public GameObject ball;
+    private double xPos;
+    private double yPos;
+    private Vector2 velocity;
+    private double mass;
+    private Vector2 force;
+    private double radius;
+    private Color color;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xPos = ball.transform.position.x;
+        yPos = ball.transform.position.y;
+        mass = 1;
+        radius = 5;
+        color = new Color(0, 1, 0, 1);
+        velocity = new Vector2(0,0);
+        force = new Vector2(0, 0);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/cameraSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraSettings : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Camera otherCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera.enabled = true;
+        otherCamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
