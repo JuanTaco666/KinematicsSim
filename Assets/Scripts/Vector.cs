5cc0aa7c64f3791054a734c61e71da5a9bb3e6c2
joohopark27 <joohopark27@gmail.com>
2020-01-12 19:50:45 -0500

    added vector prefab with script

---
+++ Assets/Scripts/Vector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector : MonoBehaviour
+{
+    public GameObject vector;
+    private Vector2 vectorValue;
+    private SpriteRenderer spriteRenderer;
+    private Color color;
+
+    public Vector(float x, float y) {
+
+        vectorValue = new Vector2(x, y);
+        color = Color.blue;
+
+        Instantiate(vector, new Vector3(0, 0, 0), Quaternion.identity);
+        spriteRenderer = vector.GetComponent<SpriteRenderer>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spriteRenderer.color = color;
+    }
+}
