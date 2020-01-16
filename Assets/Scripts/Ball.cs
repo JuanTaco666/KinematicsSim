0e435b69437f63760770ee6a476c84372bb06719
Aaron Gankin <agankin02@gmail.com>
2020-01-16 08:57:08 -0500

    fixed camera issue, and vectors are better

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,6 +6,7 @@ public class Ball : MonoBehaviour
 {
     public GameObject ball;
     public GameObject vector;
+    private Collider coll;
 
     private GameObject velocity;
     private SpriteRenderer ballRender;
@@ -15,20 +16,29 @@ public class Ball : MonoBehaviour
     private double radius;
     private float xPos;
     private float yPos;
+    private float ballBounciness;
+    private float ballFriction;
 
     // Start is called before the first frame update
     void Start()
     {
+        coll = GetComponent<Collider>();
         ballRender = GetComponent<SpriteRenderer>();
+
         xPos = ball.transform.position.x;
         yPos = ball.transform.position.y;
+
+        ballBounciness = 1;
+        ballFriction = 0;
         mass = 1;
         radius = 5;
         color = new Color(0, 1, 0, 1);
         velocity = instantiateVector(new Vector2(1, 1), "velocity");
-        force = instantiateVector(new Vector2(1, 1), "force");
+        force = instantiateVector(new Vector2(-1, 1), "force");
         ballRender.color = color;
-        
+
+        //coll.material.bounciness = ballBounciness;
+        //coll.material.dynamicFriction = ballFriction;
     }
 
     // Update is called once per frame
@@ -70,7 +80,11 @@ public class Ball : MonoBehaviour
     GameObject instantiateVector(Vector2 vector, string description) 
     {
         float angle = Vector2.Angle(new Vector2(1, 0), vector);
-        GameObject v = (GameObject)Instantiate(this.vector, new Vector3(xPos, yPos, 0), new Quaternion(0, 0, (float)angle, 1));
+        GameObject v = (GameObject)Instantiate(this.vector, new Vector3(xPos, yPos, 1), Quaternion.identity);
+        //GameObject v = (GameObject)Instantiate(this.vector);
+        //v.transform.SetParent(ball.transform);
+        //Debug.Log(ball.transform.position.x - v.transform.position.x);
+        v.transform.Rotate(0, 0, angle, Space.World);
         v.GetComponent<Vector>().setVector2(vector);
         v.GetComponent<Vector>().setObject(ball);
 
