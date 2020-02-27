d7c7bbfeddaf4f9d206ae37eb62744c5211cee59
joohopark27 <joohopark27@gmail.com>
2020-02-26 21:54:53 -0500

    subwindows are dragable, closeable

---
+++ Assets/Scripts/UI/SubWindow/Title.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Title : MonoBehaviour, IDragHandler
+{
+
+    public Button close;
+
+    [SerializeField] private RectTransform dragRectTransform;
+    [SerializeField] private Canvas canvas;
+    private GameObject panel;
+
+    private void Awake()
+    {
+
+        if(dragRectTransform == null)
+        {
+            dragRectTransform = transform.parent.GetComponent<RectTransform>();
+        }
+
+        if(canvas == null)
+        {
+            Transform testCanvasTransform = transform;
+
+            while(canvas == null){
+                testCanvasTransform = testCanvasTransform.parent;
+                canvas = testCanvasTransform.GetComponent<Canvas>();
+            }
+        }
+
+    }
+
+    void Start()
+    {
+        panel = transform.parent.gameObject;
+    
+        close.onClick.AddListener(Close);
+
+    }
+
+    void Close()
+    {
+        Destroy(panel);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        dragRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+}
