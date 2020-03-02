b330549d9fe0979034cea23ff4ffef09e2fdfc46
joohopark27 <joohopark27@gmail.com>
2020-03-02 08:07:44 -0500

    added button component

---
+++ Assets/Scripts/UI/SubWindow/SubWindowButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.UI;
+
+public class SubWindowButton : SubWindowComponent
+{
+    public Button button;
+
+    void SetText(string text)
+    {
+        button.GetComponent<Text>().text = text;
+    }
+
+    public void AddListener(UnityEngine.Events.UnityAction call)
+    {
+        button.onClick.AddListener(call);
+    }
+
+}
